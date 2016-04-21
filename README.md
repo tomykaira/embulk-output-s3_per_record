16ff7b7f0251eec9c19a98c32f5a0405ab4a8303
tomykaira <tomykaira@gmail.com>
2016-04-21 19:39:53 +0900

    First implementation

--- README.md
+++ README.md
@@ -1,28 +1,42 @@
 # S3 Per Record output plugin for Embulk
 
-TODO: Write short description here and build.gradle file.
+This plugin uploads a column's value to S3 as one S3 object per row.
+S3 object key can be composed of another column.
 
 ## Overview
 
 * **Plugin type**: output
 * **Load all or nothing**: no
 * **Resume supported**: no
-* **Cleanup supported**: yes
 
 ## Configuration
 
-- **option1**: description (integer, required)
-- **option2**: description (string, default: `"myvalue"`)
-- **option3**: description (string, default: `null`)
+- **bucket**: S3 bucket name.
+- **key**: S3 object key. `${column}` is replaced by the column's value.
+- **data_column**: Column for object's body.
+- **aws_access_key_id**: (optional) AWS access key id. If not given, [DefaultAWSCredentialsProviderChain](http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/auth/DefaultAWSCredentialsProviderChain.html) is used to get credentials.
+- **aws_secret_access_key**: (optional) AWS secret access key. Required if `aws_access_key_id` is given.
 
 ## Example
 
 ```yaml
 out:
   type: s3_per_record
-  option1: example1
-  option2: example2
+  bucket: your-bucket-name
+  key: "sample/${id}.txt"
+  data_column: payload
 ```
+
+```
+id | payload
+------------
+ 1 | hello
+ 5 | world
+12 | embulk
+```
+
+This generates `s3://your-bucket-name/sample/1.txt` with its content `hello`,
+`s3://your-bucket-name/sample/5.txt` with its content `world`, and so on.
 
 
 ## Build
--- src/main/java/org/embulk/output/s3_per_record/S3PerRecordOutputPlugin.java
+++ src/main/java/org/embulk/output/s3_per_record/S3PerRecordOutputPlugin.java
@@ -1,39 +1,68 @@
 package org.embulk.output.s3_per_record;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
-import com.google.common.base.Optional;
-import org.embulk.config.TaskReport;
+
+import javax.validation.constraints.NotNull;
+
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
 import org.embulk.config.ConfigDiff;
 import org.embulk.config.ConfigSource;
 import org.embulk.config.Task;
+import org.embulk.config.TaskReport;
 import org.embulk.config.TaskSource;
+import org.embulk.spi.Column;
 import org.embulk.spi.Exec;
 import org.embulk.spi.OutputPlugin;
-import org.embulk.spi.PageOutput;
+import org.embulk.spi.Page;
+import org.embulk.spi.PageReader;
 import org.embulk.spi.Schema;
 import org.embulk.spi.TransactionalPageOutput;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+import com.google.common.base.Optional;
+
 
 public class S3PerRecordOutputPlugin
         implements OutputPlugin
 {
+
+    public static final String KEY_COLUMN_START_MARKER = "${";
+    public static final String KEY_COLUMN_END_MARKER = "}";
+
     public interface PluginTask
             extends Task
     {
-        // configuration option 1 (required integer)
-        @Config("option1")
-        public int getOption1();
+        // S3 bucket name.
+        @Config("bucket")
+        String getBucket();
 
-        // configuration option 2 (optional string, null is not allowed)
-        @Config("option2")
-        @ConfigDefault("\"myvalue\"")
-        public String getOption2();
+        // S3 key. {{column}} is expanded to column value in a row.
+        @Config("key")
+        String getKey();
 
-        // configuration option 3 (optional string, null is allowed)
-        @Config("option3")
+        // Column name.
+        @Config("data_column")
+        String getDataColumn();
+
+        // AWS access key id.
+        @Config("aws_access_key_id")
         @ConfigDefault("null")
-        public Optional<String> getOption3();
+        Optional<String> getAwsAccessKeyId();
+
+        // AWS secret access key
+        @Config("aws_secret_access_key")
+        @ConfigDefault("null")
+        Optional<String> getAwsSecretAccessKey();
     }
 
     @Override
@@ -42,11 +71,6 @@ public class S3PerRecordOutputPlugin
             OutputPlugin.Control control)
     {
         PluginTask task = config.loadConfig(PluginTask.class);
-
-        // retryable (idempotent) output:
-        // return resume(task.dump(), schema, taskCount, control);
-
-        // non-retryable (non-idempotent) output:
         control.run(task.dump());
         return Exec.newConfigDiff();
     }
@@ -70,8 +94,153 @@ public class S3PerRecordOutputPlugin
     public TransactionalPageOutput open(TaskSource taskSource, Schema schema, int taskIndex)
     {
         PluginTask task = taskSource.loadTask(PluginTask.class);
+        S3PerRecordPageOutput output = new S3PerRecordPageOutput(task, schema);
+        output.open();
+        return output;
+    }
 
-        // Write your code here :)
-        throw new UnsupportedOperationException("S3PerRecordOutputPlugin.run method is not implemented yet");
+    private static class S3PerRecordPageOutput implements TransactionalPageOutput {
+        private final TransferManager transferManager;
+        private PageReader pageReader;
+        private final String bucket;
+        private final List<KeyPart> keyPattern;
+        private final Column dataColumn;
+        private final Schema schema;
+        private List<Upload> uploads;
+
+        public S3PerRecordPageOutput(PluginTask task, Schema schema) {
+            this.schema = schema;
+            bucket = task.getBucket();
+            keyPattern = makeKeyPattern(task.getKey());
+            dataColumn = schema.lookupColumn(task.getDataColumn());
+
+            AWSCredentials credentials;
+            if (task.getAwsAccessKeyId().isPresent() && task.getAwsSecretAccessKey().isPresent()) {
+                credentials = new BasicAWSCredentials(task.getAwsAccessKeyId().get(), task.getAwsSecretAccessKey().get());
+            } else {
+                credentials = new DefaultAWSCredentialsProviderChain().getCredentials();
+            }
+            transferManager = new TransferManager(credentials);
+
+            uploads = new ArrayList<>();
+        }
+
+        private List<KeyPart> makeKeyPattern(final String key) {
+            int offset = 0;
+            int nextOffset = 0;
+            ArrayList<KeyPart> parts = new ArrayList<>();
+            while ((nextOffset = key.indexOf(KEY_COLUMN_START_MARKER, offset)) != -1) {
+                parts.add(new ConstantStringPart(key.substring(offset, nextOffset)));
+                offset = nextOffset + KEY_COLUMN_START_MARKER.length();
+                nextOffset = key.indexOf(KEY_COLUMN_END_MARKER, offset);
+                if (nextOffset == -1) {
+                    throw new RuntimeException("Key's column name segment is not closed. Check that {{ and }} are corresponding.");
+                }
+                parts.add(new ColumnPart(schema.lookupColumn(key.substring(offset, nextOffset))));
+                offset = nextOffset + KEY_COLUMN_END_MARKER.length();
+            }
+            if (offset < key.length()) {
+                parts.add(new ConstantStringPart(key.substring(offset, key.length())));
+            }
+            return parts;
+        }
+
+        void open() {
+            pageReader = new PageReader(schema);
+        }
+
+        @Override
+        public void add(Page page) {
+            pageReader.setPage(page);
+
+            while (pageReader.nextRecord()) {
+                String key = buildKey(pageReader);
+
+                String payload = pageReader.getString(dataColumn);
+                byte[] payloadBytes = payload.getBytes();
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentLength(payloadBytes.length);
+
+                try (InputStream is = new ByteArrayInputStream(payloadBytes)) {
+                    Upload upload = transferManager.upload(bucket, key, is, metadata);
+                    uploads.add(upload);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        private String buildKey(@NotNull PageReader pageReader) {
+            StringBuilder sb = new StringBuilder();
+            for (KeyPart p : keyPattern) {
+                sb.append(p.resolve(pageReader));
+            }
+            return sb.toString();
+        }
+
+        @Override
+        public void finish()
+        {
+            try {
+                for (Upload upload : uploads) {
+                    upload.waitForUploadResult();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                close();
+            }
+        }
+
+        @Override
+        public void close()
+        {
+            if (pageReader != null) {
+                pageReader.close();
+            }
+        }
+
+        @Override
+        public void abort() {
+            for (Upload upload : uploads) {
+                upload.abort();
+            }
+        }
+
+        @Override
+        public TaskReport commit()
+        {
+            return Exec.newTaskReport();
+        }
+    }
+
+    private interface KeyPart {
+        @NotNull String resolve(PageReader reader);
+    }
+
+    private static class ConstantStringPart implements KeyPart {
+        @NotNull private final String data;
+
+        private ConstantStringPart(@NotNull String data) {
+            this.data = data;
+        }
+
+        @Override
+        public String resolve(@NotNull PageReader reader) {
+            return data;
+        }
+    }
+
+    private static class ColumnPart implements KeyPart {
+        @NotNull private final Column column;
+
+        private ColumnPart(@NotNull Column column) {
+            this.column = column;
+        }
+
+        @Override
+        public String resolve(@NotNull PageReader reader) {
+            return reader.getString(column);
+        }
     }
 }
